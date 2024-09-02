35fa46a9da5de288be6446abfd7fa611d3cb2073
OscMR23 <moreno122101@outlook.com>
2024-09-02 09:07:57 -0600

    Actualizacion

--- Parcial 1/Express/index.js
+++ Parcial 1/Express/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
  const app = express();
+//  const cors = require('cors')
 
- app.get('/',(req,res)=>{
+
+//  app.use(cors());
+   app.get('/',(req,res)=>{
     res.send('Server Express contestando a peticion get')
+    
  })
 
  app.post('/',(req,res)=>{
