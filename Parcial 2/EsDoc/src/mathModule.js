528892eb7e46e472a8874d5df88ee2557da67111
OscMR23 <moreno122101@outlook.com>
2024-10-10 22:20:08 -0600

    EsDocs

---
+++ Parcial 2/EsDoc/src/mathModule.js
@@ -0,0 +1,31 @@
+// mathModule.js
+
+/**
+ * Suma dos números.
+ * @param {number} a - El primer número.
+ * @param {number} b - El segundo número.
+ * @returns {number} - El resultado de la suma.
+ */
+export function sumar(a, b) {
+  return a + b;
+}
+
+/**
+* Resta dos números.
+* @param {number} a - El número al que se le va a restar.
+* @param {number} b - El número que se resta.
+* @returns {number} - El resultado de la resta.
+*/
+export function restar(a, b) {
+  return a - b;
+}
+
+/**
+* Multiplica dos números.
+* @param {number} a - El primer número.
+* @param {number} b - El segundo número.
+* @returns {number} - El resultado de la multiplicación.
+*/
+export function multiplicar(a, b) {
+  return a * b;
+}
