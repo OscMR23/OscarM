9dc903fd352952238f4efa91d849abc8b43af183
OscMR23 <moreno122101@outlook.com>
2024-11-22 10:09:50 -0500

    OpenApiLove

--- Parcial 3/OpenApi/index.js
+++ Parcial 3/OpenApi/index.js
@@ -4,16 +4,19 @@ const app = express();
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc"); // Crea una instancia de la aplicación Express
 const cors = require("cors"); // Importa el módulo CORS
+
+// Configura el puerto
 const port = process.env.PORT || 8082;
 
-
-
+// Middleware para habilitar CORS
 app.use(cors()); // Habilita CORS para permitir solicitudes de otros dominios, si es necesario
 
+// Ruta de ejemplo
 app.get("/empleado", (req, res) => {
   res.json({ message: "Servidor funcionando en 8082" }); // Responde con un mensaje JSON
 });
 
+// Configuración de Swagger
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
@@ -21,26 +24,25 @@ const swaggerOptions = {
       title: "API Tareas",
       version: "1.0.0",
     },
-    servers: [{ url: "http://localhost:${port}" }],
+    servers: [
+      {
+        url: `https://oscarm-production.up.railway.app/`, // Usa la URL de Railway
+      },
+    ],
   },
   apis: [`${path.join(__dirname, "index.js")}`],
 };
 
-/**
- * @swagger
- * /empleado:
- *   get:
- *     description: Consultar todos los empleados
- *     responses:
- *       200:
- *          description: Regresa un arreglo de objetos con los empleados.
- */
-
+// Generación de la documentación Swagger
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// Ruta para acceder a la documentación de Swagger
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-app.get("/api-spec",(req,res)=>{
-  res.json(swaggerDocs)
-})
+// Ruta para obtener la especificación completa de la API
+app.get("/api-spec", (req, res) => {
+  res.json(swaggerDocs);
+});
 
-app.listen(port, () => console.log("Server is listening on ${port}")); // Inicia el servidor en el puerto 8082
+// Inicia el servidor
+app.listen(port, () => console.log(`Server is listening on ${port}`)); // Usa comillas invertidas para interpolación
